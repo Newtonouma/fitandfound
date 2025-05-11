63ef80936d9794a067e3518f7153a4dddc497c1c
Newtonouma <armstrongnewton02@gmail.com>
2025-05-11 13:45:52 +0300

    changes made successfully

--- src/components/OurServices/services/services.jsx
+++ src/components/OurServices/services/services.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./Services.css";
+import "./services.css";
 
 // Import local images
 import tailoringImg from "/ourservices/servicesimages/1.jpg";
